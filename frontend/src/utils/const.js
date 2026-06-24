668983d31cf833e48f9fd01a57ed353b50d9c49d
Akash Yadav <ay2484900@gmail.com>
2026-06-25 01:29:17 +0530

    added url

--- frontend/src/utils/const.js
+++ frontend/src/utils/const.js
@@ -1,6 +1,5 @@
-// Use absolute URLs in production, relative URLs in development
-
-const API_BASE = 'http://localhost:8000'
+// Use environment variable for backend base URL
+const API_BASE = import.meta.env.VITE_API_BASE
 
 export const JOB_API_END_POINT = `${API_BASE}/job`;
 export const APPLICATION_API_END_POINT = `${API_BASE}/application`;
