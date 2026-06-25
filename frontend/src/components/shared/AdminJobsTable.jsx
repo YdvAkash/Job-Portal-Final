5ead094b6bbc9d6a50f0e2e6d90e052a9b696473
Ubuntu <ubuntu@ip-172-31-9-117.ap-south-1.compute.internal>
2026-06-25 09:47:09 +0000

    asdfasfa

--- frontend/src/components/shared/AdminJobsTable.jsx
+++ frontend/src/components/shared/AdminJobsTable.jsx
@@ -97,7 +97,7 @@ const AdminJobsTable = () => {
             <TableRow key={job._id}>
               <TableCell>{job?.company?.name}</TableCell>
               <TableCell>{job?.title}</TableCell>
-              <TableCell>{job?.createdAt.split("T")[0]}</TableCell>
+              <TableCell>{job?.createdAt?.split("T")[0] || ""}</TableCell>
               <TableCell className="text-right cursor-pointer">
                 <Popover>
                   <PopoverTrigger>
